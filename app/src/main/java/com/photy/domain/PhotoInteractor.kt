c8df255c36fc74d5d9364c4c37c19515ee27e7b4
Alexey Sadykov <alexey.sadykov@altarix.ru>
2016-09-03 12:34:15 +0400

    implement load photos methods by feature

--- app/src/main/java/com/photy/domain/PhotoInteractor.kt
+++ app/src/main/java/com/photy/domain/PhotoInteractor.kt
@@ -2,10 +2,12 @@ package com.photy.domain
 
 import com.photy.BuildConfig.CONSUMER_KEY
 import com.photy.data.entity.Feature
+import com.photy.data.entity.Feature.*
 import com.photy.data.entity.Photo
-import com.photy.data.entity.Sort
+import com.photy.data.entity.PhotosResponse
 import com.photy.data.repository.PhotoRepository
 import com.photy.ext.arrayMapOf
+import rx.Single
 import rx.Single.just
 import rx.SingleSubscriber
 import javax.inject.Inject
@@ -17,22 +19,56 @@ class PhotoInteractor @Inject constructor(
   private var loadMore = true
   private var isLoading = false
 
-  fun loadPopularPhotos(subscriber: SingleSubscriber<List<Photo>>, sortBy: Sort = Sort.CREATED_AT) {
-    if (! loadMore) return
-    if (isLoading) return
+  fun loadPopularPhotos(subscriber: SingleSubscriber<List<Photo>>) {
+    load(POPULAR, subscriber)
+  }
 
-    isLoading = true
-    execute(photoRepository.photos(arrayMapOf(
-        "feature" to Feature.POPULAR.name,
-        "sort" to sortBy.name,
-        "image_size" to "21",
-        "page" to currentPage.toString(),
-        "consumer_key" to CONSUMER_KEY))
-                .flatMap {
-                  this.isLoading = false
-                  this.loadMore = currentPage <= it.totalPages
-                  this.currentPage ++
-                  return@flatMap just(it.photos)
-                }, subscriber)
+  fun loadUpcomingPhotos(subscriber: SingleSubscriber<List<Photo>>) {
+    load(UPCOMING, subscriber)
+  }
+
+  fun loadFreshTodayPhotos(subscriber: SingleSubscriber<List<Photo>>) {
+    load(FRESH_TODAY, subscriber)
+  }
+
+  fun loadFreshWeekPhotos(subscriber: SingleSubscriber<List<Photo>>) {
+    load(FRESH_WEEK, subscriber)
+  }
+
+  fun loadFreshYesterdayPhotos(subscriber: SingleSubscriber<List<Photo>>) {
+    load(FRESH_YESTERDAY, subscriber)
+  }
+
+  private fun load(feature: Feature, subscriber: SingleSubscriber<List<Photo>>) {
+    if (needLoaded()) {
+      execute(transformData(photoRepository.photos(arrayMapOf(
+          "feature" to feature.name,
+          "image_size" to "21",
+          "page" to currentPage.toString(),
+          "consumer_key" to CONSUMER_KEY))), subscriber)
+    }
+  }
+
+  private fun needLoaded(): Boolean {
+    if (loadMore && ! isLoading) {
+      isLoading = true
+      return true
+    } else {
+      return false
+    }
+  }
+
+  private fun transformData(stream: Single<PhotosResponse>) =
+      stream.flatMap {
+        this.isLoading = false
+        this.loadMore = currentPage <= it.totalPages
+        this.currentPage ++
+        return@flatMap just(it.photos)
+      }
+
+  private fun flushData() {
+    currentPage = 1
+    loadMore = true
+    isLoading = false
   }
 }
